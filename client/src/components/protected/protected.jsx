bdc9abf44bef805fc4bacd10d7cd82973fabb5f5
manojkmishra <manojmishra571980@gmail.com>
2022-08-14 01:21:47 +1000

    Protected caps issue

--- client/src/components/protected/protected.jsx
+++ client/src/components/protected/protected.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useValue } from '../../context/ContextProvider';
 import AccessMessage from './AccessMessage';
-
+//file name changed
 const Protected = ({ children }) => {
   const {
     state: { currentUser },
